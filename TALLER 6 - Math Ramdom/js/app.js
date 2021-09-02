76474f1aa53b4777ad379302386ce78c38a7b12c
Elian-jaramillo-rojas <ejaramillo62@misena.edu.co>
2021-09-01 19:52:08 -0500

    Se mejoro el codigo JS haciendolo mas corto y optimizado

--- TALLER 6 - Math Ramdom/js/app.js
+++ TALLER 6 - Math Ramdom/js/app.js
@@ -8,11 +8,7 @@ function CambiarColor(){
     let G = valorRGB(0, 255)
     let B = valorRGB(0, 255)
 
-    let fondo = container;
-    let colorFondo = ""
-
     container.style.backgroundColor = "rgb(" + R + "," + G + "," + B + ")";
-    // container.style.backgroundColor = "rgb(40, 10, 30)";
 };
 
 function valorRGB(min, max) {
